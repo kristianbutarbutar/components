6a3d4dc668c5d8fe7d89e41f3345b19c377d343d
kristianbutarbutar <kristian.butarbutar@gmail.com>
2018-07-29 00:56:10 +0700

    intial2-29.07.2018

---
+++ dynamicore/src/tao/dao/DaoTestMain.java
@@ -0,0 +1,7 @@
+package tao.dao;
+
+public class DaoTestMain {
+	public static void main(String...strings){
+		System.out.println("Static test main class in DaoTestMain");
+	}
+}
---
+++ dynamicore/src/tao/form/FormTestMain.java
@@ -0,0 +1,7 @@
+package tao.form;
+
+public class FormTestMain {
+	public static void main(String...strings){
+		System.out.println("Static test main class in FormTestMain");
+	}
+}
