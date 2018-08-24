97048dea4de8e34a88934a0b8d1355adee276c92
kristianbutarbutar <kristian.butarbutar@gmail.com>
2018-08-25 04:12:23 +0700

    update

--- dynamicweb/src/tao/core/ctrl/sys/FormColumnCtrl.java
+++ dynamicweb/src/tao/core/ctrl/sys/FormColumnCtrl.java
@@ -4,7 +4,6 @@ import org.apache.log4j.Logger;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import tao.dao.core.BaseFormColumnsDAO;
-import tao.dao.core.BaseFormsDAO;
 import tao.vo.BColumnVO;
 
 @Controller
@@ -20,8 +18,13 @@ import tao.vo.BColumnVO;
 public class FormColumnCtrl {
 	private static final Logger logger = Logger.getLogger(FormColumnCtrl.class);
 	
-	@RequestMapping(value="/ui/sys/formcolumns/{formid}", method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value="/ui/sys/formcolumns/{formid}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody BColumnVO getForms(@PathVariable String formid) {
 		return (new BaseFormColumnsDAO()).getFormColumns(formid);
+	}
+	
+	@RequestMapping(value="/ui/sys/formcolumns/{formid}/{colid}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody String unlinkFormColumn(@PathVariable String formid, @PathVariable String colid) {
+		return (new BaseFormColumnsDAO()).deleteFormColumns(formid, colid);
 	}
 }
