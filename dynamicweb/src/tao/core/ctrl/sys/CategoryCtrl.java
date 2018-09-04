cfb0a0df9af33a8601851a87baa2c79b22e671eb
kristianbutarbutar <kristian.butarbutar@gmail.com>
2018-09-05 04:44:50 +0700

    update

--- dynamicweb/src/tao/core/ctrl/sys/CategoryCtrl.java
+++ dynamicweb/src/tao/core/ctrl/sys/CategoryCtrl.java
@@ -1,7 +1,9 @@
 package tao.core.ctrl.sys;
 
 import org.apache.log4j.Logger;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,27 +21,40 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public class CategoryCtrl {
 	private static final Logger logger = Logger.getLogger(CategoryCtrl.class);
 	
-	@RequestMapping(value="/ui/sys/category/{name}", method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody BCategoryVO getForms(@PathVariable String name) {
-		return (new BaseCategoryDAO()).getCategories(name);
+	@RequestMapping(value="/ui/sys/category/{name}/{code}", method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody BCategoryVO getCategorys(@PathVariable String name, @PathVariable String code) {
+		String n="null", l="null";
+		if(!name.equalsIgnoreCase("-")) n = name;
+		if(!code.equalsIgnoreCase("-")) l = code;
+		return (new BaseCategoryDAO()).getCategories(n, l);
 	}
 	
-	@RequestMapping(value="/ui/sys/category/add", method = RequestMethod.POST)
-	public String addForm(@RequestBody BCategoryVO vo) {
+	@RequestMapping(value="/ui/sys/category/add", method = RequestMethod.POST, consumes =
+		 {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_JSON_VALUE},
+		 produces={MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_JSON_VALUE})
+	@ResponseBody ResponseEntity  addCategory(@RequestBody BCategoryVO vo) {
 		(new BaseCategoryDAO()).addCategory(vo);  
-        return "success";
+        return ResponseEntity.ok(HttpStatus.OK);
 	}
 	
-	@RequestMapping(value="/ui/sys/category/update", method = RequestMethod.POST)
-	public String updateForm(@RequestBody BCategoryVO vo) {
-		(new BaseCategoryDAO()).updateCategory(vo);  
-        return "success";
+	@RequestMapping(value="/ui/sys/category/update", method = RequestMethod.POST, consumes =
+		 {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_JSON_VALUE},
+		 produces={MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_JSON_VALUE})
+	@ResponseBody ResponseEntity  updateCategory(@RequestBody BCategoryVO vo) {
+		vo.setUpdatedBy("SYS");
+		vo.setCreatedBy("SYS");
+		if(vo.getCid()!=null && !vo.getCid().equals(""))
+			(new BaseCategoryDAO()).updateCategory(vo);
+		else addCategory(vo);
+        return ResponseEntity.ok(HttpStatus.OK);
 	}
 	
-	@RequestMapping(value="/ui/sys/category/drop", method = RequestMethod.POST)
-	public String deleteForm(@RequestBody BCategoryVO vo) {
+	@RequestMapping(value="/ui/sys/category/drop", method = RequestMethod.POST, consumes =
+		 {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_JSON_VALUE},
+		 produces={MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_JSON_VALUE})
+	@ResponseBody ResponseEntity  deleteCategory(@RequestBody BCategoryVO vo) {
 		(new BaseCategoryDAO()).deleteCategory(vo);  
-        return "success";
+        return ResponseEntity.ok(HttpStatus.OK);
 	}
 	
 }
--- dynamicweb/src/tao/core/ctrl/sys/ColumnCtrl.java
+++ dynamicweb/src/tao/core/ctrl/sys/ColumnCtrl.java
@@ -1,7 +1,9 @@
 package tao.core.ctrl.sys;
 
 import org.apache.log4j.Logger;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,27 +21,40 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public class ColumnCtrl {
 	private static final Logger logger = Logger.getLogger(ColumnCtrl.class);
 	
-	@RequestMapping(value="/ui/sys/column/{name}", method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody BColumnVO getLists(@PathVariable String name) {
-		return (new BaseColumnsDAO()).getColumns(name);
+	@RequestMapping(value="/ui/sys/column/{name}/{label}", method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody BColumnVO getColumns(@PathVariable String name, @PathVariable String label) {
+		String n="null", l="null";
+		if(!name.equalsIgnoreCase("-")) n = name;
+		if(!label.equalsIgnoreCase("-")) l = label;
+		return (new BaseColumnsDAO()).getColumns(n,l);
 	}
 	
-	@RequestMapping(value="/ui/sys/column/add", method = RequestMethod.POST)
-	public String addList(@RequestBody BColumnVO vo) {
+	@RequestMapping(value="/ui/sys/column/add", method = RequestMethod.POST, consumes =
+		 {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_JSON_VALUE},
+		 produces={MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_JSON_VALUE})
+	public @ResponseBody ResponseEntity addColumn(@RequestBody BColumnVO vo) {
 		(new BaseColumnsDAO()).addColumn(vo);  
-        return "success";
+         return ResponseEntity.ok(HttpStatus.OK);
 	}
 	
-	@RequestMapping(value="/ui/sys/column/update", method = RequestMethod.POST)
-	public String updateList(@RequestBody BColumnVO vo) {
-		(new BaseColumnsDAO()).updateColumn(vo);  
-        return "success";
+	@RequestMapping(value="/ui/sys/column/update", method = RequestMethod.POST, consumes =
+		 {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_JSON_VALUE},
+		 produces={MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_JSON_VALUE})
+	public @ResponseBody ResponseEntity updateColumn(@RequestBody BColumnVO vo) {
+		vo.setUpdatedBy("SYS");
+		vo.setCreatedBy("SYS");
+		if(vo.getCid()!=null && !vo.getCid().equals(""))
+			(new BaseColumnsDAO()).updateColumn(vo);
+		else addColumn(vo);
+		return ResponseEntity.ok("success");
 	}
 	
-	@RequestMapping(value="/ui/sys/column/drop", method = RequestMethod.POST)
-	public String deleteList(@RequestBody BColumnVO vo) {
-		(new BaseColumnsDAO()).deleteColumn(vo);  
-        return "success";
+	@RequestMapping(value="/ui/sys/column/drop", method = RequestMethod.POST,consumes =
+		 {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_JSON_VALUE},
+		 produces={MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_JSON_VALUE})
+	public @ResponseBody ResponseEntity deleteColumn(@RequestBody BColumnVO vo) {
+		(new BaseColumnsDAO()).deleteColumn(vo);
+		return ResponseEntity.ok("success");
 	}
 	
 }
